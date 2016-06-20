08efe6fa76bdc8e84abbc828c3de2c99960315bf
nyaman <n.yamanaka@miraibox.jp>
2016-06-20 18:55:44 +0900

    現在地測定ボタン追加、測定回数表示ラベルを追加

--- maptest/ViewController.swift
+++ maptest/ViewController.swift
@@ -1,25 +1,162 @@
 //
 //  ViewController.swift
-//  maptest
-//
-//  Created by miraibox on 2016/06/20.
-//  Copyright © 2016年 miraibox. All rights reserved.
+//  MapKit003
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    var myMapView: MKMapView!
+    var myLocationManager: CLLocationManager!
+    
+    var myLabel:UILabel!
 
+    var locationCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // ボタンの生成.
+        let myButton = UIButton(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        myButton.backgroundColor = UIColor.orangeColor()
+        myButton.layer.masksToBounds = true
+        myButton.setTitle("Do", forState: .Normal)
+        myButton.layer.cornerRadius = 20.0
+        myButton.layer.position = CGPoint(x: self.view.bounds.width-40, y:self.view.bounds.height-40)
+        myButton.addTarget(self, action: "onClickMyButton:", forControlEvents: .TouchUpInside)
+        
+        // LocationManagerの生成.
+        myLocationManager = CLLocationManager()
+        
+        // Delegateの設定.
+        myLocationManager.delegate = self
+        
+        // 距離のフィルタ.
+        myLocationManager.distanceFilter = 100.0
+        
+        // 精度.
+        myLocationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        
+        // セキュリティ認証のステータスを取得.
+        let status = CLLocationManager.authorizationStatus()
+        
+        // まだ認証が得られていない場合は、認証ダイアログを表示.
+        if(status == CLAuthorizationStatus.NotDetermined) {
+            
+            // まだ承認が得られていない場合は、認証ダイアログを表示.
+            self.myLocationManager.requestAlwaysAuthorization();
+        }
+        
+        // 位置情報の更新を開始.
+        myLocationManager.startUpdatingLocation()
+        
+        // MapViewの生成.
+        myMapView = MKMapView()
+        
+        // MapViewのサイズを画面全体に.
+        myMapView.frame = self.view.bounds
+        
+        // Delegateを設定.
+        myMapView.delegate = self
+        
+        // MapViewをViewに追加.
+        self.view.addSubview(myMapView)
+        
+        // 中心点の緯度経度.
+        let myLat: CLLocationDegrees = 37.506804
+        let myLon: CLLocationDegrees = 139.930531
+        let myCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(myLat, myLon) as CLLocationCoordinate2D
+        
+        // 縮尺.
+        let myLatDist : CLLocationDistance = 100
+        let myLonDist : CLLocationDistance = 100
+        
+        // Regionを作成.
+        let myRegion: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(myCoordinate, myLatDist, myLonDist);
+        
+        // MapViewに反映.
+        myMapView.setRegion(myRegion, animated: true)
+        
+        myLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 30))
+        myLabel.layer.position = CGPoint(x: self.view.bounds.width/2, y:100.0)
+//        myLabel.text = locationCount.description+"回測定しました"
+//        myLabel.textAlignment = NSTextAlignment.Center
+        self.view.addSubview(myButton)
+//        self.view.addSubview(myLabel)
+    }
+    
+    // ボタンイベントのセット.
+    func onClickMyButton(sender: UIButton){
+        // 現在位置の取得を開始.
+        myLocationManager.startUpdatingLocation()
+    }
+    
+    // GPSから値を取得した際に呼び出されるメソッド.
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // 配列から現在座標を取得.
+        let myLocations: NSArray = locations as NSArray
+        let myLastLocation: CLLocation = myLocations.lastObject as! CLLocation
+        let myLocation:CLLocationCoordinate2D = myLastLocation.coordinate
+        
+        // 縮尺.
+        let myLatDist : CLLocationDistance = 100
+        let myLonDist : CLLocationDistance = 100
+        
+        // Regionを作成.
+        let myRegion: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(myLocation, myLatDist, myLonDist);
+        
+        locationCount++
+        
+        // MapViewに反映.
+        myMapView.setRegion(myRegion, animated: true)
+        myLabel.text = locationCount.description+"回測定しました"
+        myLabel.textAlignment = NSTextAlignment.Center
+        self.view.addSubview(myLabel)
+        putPin(myLastLocation)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //ピンを立てる関数
+    func putPin(myLastLocation: CLLocation) {
+        // ピンを生成.
+        let myPin: MKPointAnnotation = MKPointAnnotation()
+        // 中心点.
+        let center: CLLocationCoordinate2D = CLLocationCoordinate2DMake(myLastLocation.coordinate.latitude, myLastLocation.coordinate.longitude)
+        // 座標を設定.
+        myPin.coordinate = center
+        // タイトルを設定.
+        myPin.title = "精度 "+myLastLocation.horizontalAccuracy.description
+        // サブタイトルを設定.
+        myPin.subtitle = "緯度 "+myLastLocation.coordinate.latitude.description+"¥n"+"経度 "+myLastLocation.coordinate.longitude.description
+        
+        // MapViewにピンを追加.
+        myMapView.addAnnotation(myPin)
     }
-
-
+    
+    // Regionが変更した時に呼び出されるメソッド.
+    func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        print("regionDidChangeAnimated")
+    }
+    
+    // 認証が変更された時に呼び出されるメソッド.
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        switch status{
+        case .AuthorizedWhenInUse:
+            print("AuthorizedWhenInUse")
+        case .Authorized:
+            print("Authorized")
+        case .Denied:
+            print("Denied")
+        case .Restricted:
+            print("Restricted")
+        case .NotDetermined:
+            print("NotDetermined")
+        default:
+            print("etc.")
+        }
+    }
+    
 }
-
