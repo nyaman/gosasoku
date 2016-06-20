1b48f0b50ecf6e72fa82195cdb9ab83b2e10412f
nyaman <n.yamanaka@miraibox.jp>
2016-06-20 16:38:02 +0900

    Initial Commit

---
+++ maptest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  maptest
+//
+//  Created by miraibox on 2016/06/20.
+//  Copyright © 2016年 miraibox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
